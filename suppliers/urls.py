cdd990d970c0f377a9b511aa64bd9b82c784a604
snigdha <snigdhae7@gmail.com>
2026-06-06 08:37:22 +0600

    Fixed all templates - real data connected

--- suppliers/urls.py
+++ suppliers/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.supplier_list, name='supplier_list'),
     path('add/', views.supplier_add, name='supplier_add'),
-    path('edit/<int:pk>/', views.supplier_edit, name='supplier_edit'),
-    path('delete/<int:pk>/', views.supplier_delete, name='supplier_delete'),
+    path('edit/<int:id>/', views.supplier_edit, name='supplier_edit'),
+    path('delete/<int:id>/', views.supplier_delete, name='supplier_delete'),
 ]
--- suppliers/views.py
+++ suppliers/views.py
@@ -1,32 +1,31 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Supplier
 
 def supplier_list(request):
     suppliers = Supplier.objects.all()
     return render(request, 'suppliers/supplier_list.html', {'suppliers': suppliers})
 
+
 def supplier_add(request):
     if request.method == 'POST':
         name = request.POST['name']
-        email = request.POST['email']
         phone = request.POST['phone']
-        address = request.POST['address']
-        Supplier.objects.create(name=name, email=email, phone=phone, address=address)
+        Supplier.objects.create(name=name, phone=phone)
         return redirect('supplier_list')
     return render(request, 'suppliers/supplier_form.html')
 
-def supplier_edit(request, pk):
-    supplier = get_object_or_404(Supplier, pk=pk)
+
+def supplier_edit(request, id):
+    supplier = get_object_or_404(Supplier, id=id)
     if request.method == 'POST':
         supplier.name = request.POST['name']
-        supplier.email = request.POST['email']
         supplier.phone = request.POST['phone']
-        supplier.address = request.POST['address']
         supplier.save()
         return redirect('supplier_list')
     return render(request, 'suppliers/supplier_form.html', {'supplier': supplier})
 
-def supplier_delete(request, pk):
-    supplier = get_object_or_404(Supplier, pk=pk)
+
+def supplier_delete(request, id):
+    supplier = get_object_or_404(Supplier, id=id)
     supplier.delete()
     return redirect('supplier_list')
